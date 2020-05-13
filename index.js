6637f07889f6e75fb14e6c28144b92d4e8f7e06c
Kevin <kevinrx25@gmail.com>
2020-05-13 12:16:33 +0100

    OrderList & OrderContract created

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ SPDX-License-Identifier: Apache-2.0
 
 'use strict';
 
-const fruitcontract = require('./lib/fruitContract.js');
-module.exports.contracts = [fruitcontract];
+const orderContract = require('./lib/orderContract.js');
+module.exports.contracts = [orderContract];
